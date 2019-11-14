70dd7276fcb99751279637103753d287a4763ff8
tobias-fyi <tobyreaper@gmail.com>
2019-11-14 13:37:00 -0700

    Updated model id fields with BigInteger datatype

--- deftwit/models.py
+++ deftwit/models.py
@@ -13,7 +13,7 @@ class User(DB.Model):
 
     id = DB.Column(DB.BigInteger, primary_key=True)
     handle = DB.Column(DB.String(32), nullable=False)
-    intro = DB.Column(DB.String(240), nullable=True)
+    intro = DB.Column(DB.String(500), nullable=True)
     newest_tweet_id = DB.Column(DB.BigInteger, nullable=True)
     # Look for other useful info to include
 
@@ -24,9 +24,9 @@ class User(DB.Model):
 class Tweet(DB.Model):
     """A general class for a user's tweets, gathered from Twitter."""
 
-    id = DB.Column(DB.Integer, primary_key=True)
+    id = DB.Column(DB.BigInteger, primary_key=True)
     body = DB.Column(DB.Unicode(240))
-    user_id = DB.Column(DB.Integer, DB.ForeignKey("user.id"), nullable=False)
+    user_id = DB.Column(DB.BigInteger, DB.ForeignKey("user.id"), nullable=False)
     user = DB.relationship("User", backref=DB.backref("tweets", lazy=True))
     # TODO: add a date_posted field
 
